53d640ae8a88c2b63c81ca03c29645b4e20f96c8
Sebastian Faller <ubesa@student.kit.edu>
2018-01-15 22:46:31 +0100

    Ran formatter

--- public/javascript/TaskPageCtrl.js
+++ public/javascript/TaskPageCtrl.js
@@ -1,29 +1,35 @@
 angular.module('routingApp').controller('TaskPageCtrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
     $scope.tasks = [];
     $scope.line = "";
-    console.log("Der token ist "+$window.sessionStorage.token);
-    $http.post("/gettasks", {token: $window.sessionStorage.token}).then(function(res){
+    console.log("Der token ist " + $window.sessionStorage.token);
+    $http.post("/gettasks", {
+        token: $window.sessionStorage.token
+    }).then(function(res) {
         $scope.tasks = res.data;
-    }, function(err){
+    }, function(err) {
 
     });
     $scope.addTask = function() {
-        $http.post("/addtask", {task : $scope.line})
+        $http.post("/addtask", {
+                task: $scope.line
+            })
             .then(function(response) {
                 $scope.tasks.push($scope.line);
-            }, function errorHandling(response){
+            }, function errorHandling(response) {
                 console.log(response);
             });
 
     };
-    $scope.deleteTask = function(task){
-        $http.post("/deletetask", {toDelete: task}).then(function(response){
+    $scope.deleteTask = function(task) {
+        $http.post("/deletetask", {
+            toDelete: task
+        }).then(function(response) {
             var i = $scope.tasks.indexOf(task);
-            if(i < 0) console.log("not found");
-            $scope.tasks.splice(i,1);
+            if (i < 0) console.log("not found");
+            $scope.tasks.splice(i, 1);
             console.log($scope.tasks);
-        }, function errorHandling(response){
+        }, function errorHandling(response) {
             console.log(response);
-        }); 
+        });
     };
 }]);
